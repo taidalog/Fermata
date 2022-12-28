022f31cba754afb58a4aa56e5716e91fef283005
taidalog <72747279+taidalog@users.noreply.github.com>
2022-12-29 01:04:53 +0900

    Added `Regex` module and functions. #3

---
+++ Fermata/Regex.fs
@@ -0,0 +1,16 @@
+namespace Fermata
+
+open System.Text.RegularExpressions
+
+module Regex =
+    let match' pattern input : Match =
+        Regex.Match(input, pattern)
+    
+    let matches pattern input : MatchCollection =
+        Regex.Matches(input, pattern)
+    
+    let isMatch pattern input : bool =
+        Regex.IsMatch(input, pattern)
+    
+    let replace (pattern : string) (replacement : string) (input : string) : string=
+        Regex.Replace(input, pattern, replacement)
