89c70df91ea3fb6157fb49f7c081f39a2e7ab9a2
taidalog <72747279+taidalog@users.noreply.github.com>
2022-12-28 22:51:37 +0900

    Added `RadixConversion` module and functions. #3

--- Fermata/RadixConversion.fs
+++ Fermata/RadixConversion.fs
@@ -1,5 +1,46 @@
 ﻿namespace Fermata
 
-module Print =
-    let fn x =
-        printfn "%s" x
+module RadixConversion =
+    module Dec =
+        let validate input =
+            System.Text.RegularExpressions.Regex.IsMatch(input, "^[0-9]+$")
+        
+        let toBin (input : int) =
+            System.Convert.ToString(input, 2)
+        
+        let tryToBin input =
+            match input |> validate with
+            | true -> input |> int |> toBin |> Some
+            | false -> None
+        
+        let toHex (input : int) =
+            System.Convert.ToString(input, 16)
+        
+        let tryToHex input =
+            match input |> validate with
+            | true -> input |> int |> toHex |> Some
+            | false -> None
+    
+    module Bin =
+        let validate input =
+            System.Text.RegularExpressions.Regex.IsMatch(input, "^[01]+$")
+        
+        let toDec input =
+            System.Convert.ToInt32(input, 2)
+        
+        let tryToDec input =
+            match input |> validate with
+            | true -> input |> toDec |> Some
+            | false -> None
+        
+    module Hex =
+        let validate input =
+            System.Text.RegularExpressions.Regex.IsMatch(input, "^[0-9A-Fa-f]+$")
+        
+        let toDec input =
+            System.Convert.ToInt32(input, 16)
+        
+        let tryToDec input =
+            match input |> validate with
+            | true -> input |> toDec |> Some
+            | false -> None
