6f35eaf6d6ee1081743ee20c190454c3d3ffc71e
taidalog <72747279+taidalog@users.noreply.github.com>
2023-08-12 19:23:26 +0900

    Wrote tests for `Regex` module. #45

---
+++ Fermata.Tests/Regex.Tests.fs
@@ -0,0 +1,34 @@
+module Regex.Tests
+
+open Xunit
+open Fermata
+
+[<Fact>]
+let ``Regex.match 1`` () =
+    let m: System.Text.RegularExpressions.Match = "F#" |> Regex.match' "^[A-Z]#$"
+    let actual = m.Success
+    let expected = true
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``Regex.matches 1`` () =
+    let ms: System.Text.RegularExpressions.MatchCollection =
+        "Shinkansen" |> Regex.matches "[aeiou]n"
+
+    let actual: seq<string> =
+        ms |> Seq.map (fun (x: System.Text.RegularExpressions.Match) -> x.Value)
+
+    let expected: seq<string> = seq [ "in"; "an"; "en" ]
+    Assert.Equal<seq<string>>(expected, actual)
+
+[<Fact>]
+let ``Regex.isMatch 1`` () =
+    let actual = "F#" |> Regex.isMatch "^[A-Z]#$"
+    let expected = true
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``Regex.replace 1`` () =
+    let actual = "C#" |> Regex.replace "^[A-Z](?=#$)" "F"
+    let expected = "F#"
+    Assert.Equal(expected, actual)
