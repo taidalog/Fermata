9ea4c5556256ed3227d5e4e0f9b8ed3ad2808cb8
taidalog <72747279+taidalog@users.noreply.github.com>
2023-08-13 01:49:33 +0900

    Wrote tests for `Result` module. #45

---
+++ Fermata.Tests/Result.Tests.fs
@@ -0,0 +1,20 @@
+module Result.Tests
+
+open Xunit
+open Fermata
+
+[<Fact>]
+let ``Result.ofOption 1`` () =
+    let option = Some 42
+    let msg = "The input is not a number."
+    let actual = Result.ofOption msg option
+    let expected = Ok 42
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``Result.ofOption 2`` () =
+    let option: int option = None
+    let msg = "The input is not a number."
+    let actual = Result.ofOption msg option
+    let expected = Error "The input is not a number."
+    Assert.Equal(expected, actual)
