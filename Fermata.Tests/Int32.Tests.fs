34890c0c4662800d3848975e9d5f34aac569bb4b
taidalog <72747279+taidalog@users.noreply.github.com>
2023-08-13 18:10:17 +0900

    Wrote more tests for `Int32` module. #43

--- Fermata.Tests/Int32.Tests.fs
+++ Fermata.Tests/Int32.Tests.fs
@@ -14,6 +14,39 @@ let ``Int32.validate 2`` () =
     let actual = Int32.validate "42."
 
     let expected =
-        Error <| Exceptions.Format "The input string '42.' was not in a correct format."
+        Error(Exceptions.Format "The input string '42.' was not in a correct format.")
+
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``Int32.validate 3`` () =
+    let actual = Int32.validate null
+
+    let expected =
+        Error(Exceptions.ArgumentNull "Value cannot be null. (Parameter 's')")
+
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``Int32.validate 4`` () =
+    let actual = Int32.validate ""
+    let expected = Error(Exceptions.EmptyString "Value cannot be empty string.")
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``Int32.validate 5`` () =
+    let actual = Int32.validate "2147483648" // System.Int32.MaxValue + 1
+
+    let expected =
+        Error(Exceptions.Overflow "Value was either too large or too small for an Int32.")
+
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``Int32.validate 6`` () =
+    let actual = Int32.validate "-2147483649" // System.Int32.MinValue - 1
+
+    let expected =
+        Error(Exceptions.Overflow "Value was either too large or too small for an Int32.")
 
     Assert.Equal(expected, actual)
