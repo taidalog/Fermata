8de7f089e541a7cb3c5ba62fb083beddec719df5
taidalog <72747279+taidalog@users.noreply.github.com>
2023-08-15 02:08:30 +0900

    Added a copyright notice to all files. Close #55.
    A copyright notice has been added to the beginning of all files.

--- Fermata.Tests/Validators.Tests.fs
+++ Fermata.Tests/Validators.Tests.fs
@@ -1,3 +1,9 @@
+// Fermata Version 0.5.0
+// https://github.com/taidalog/Fermata
+// Copyright (c) 2022-2023 taidalog
+// This software is licensed under the MIT License.
+// https://github.com/taidalog/Fermata/blob/main/LICENSE
+
 module Validators.Tests
 
 open Xunit
