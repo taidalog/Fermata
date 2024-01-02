8c1c3621e33c5423385e269180c3701270125390
taidalog <72747279+taidalog@users.noreply.github.com>
2024-01-03 03:38:56 +0900

    Removed `Fermata.Exceptions` from `Int32` module. #72

--- Fermata/Int32.fs
+++ Fermata/Int32.fs
@@ -11,13 +11,12 @@ open System
 [<RequireQualifiedAccess>]
 module Int32 =
     let validate (value: string) : Result<int, exn> =
-        if value = String.Empty then
-            Error(Exceptions.EmptyString "Value cannot be empty string.")
-        else
-            try
-                Ok(Int32.Parse value)
-            with
-            | :? System.ArgumentNullException as e -> Error(Exceptions.ArgumentNull e.Message)
-            | :? System.FormatException as e -> Error(Exceptions.Format e.Message)
-            | :? System.OverflowException as e -> Error(Exceptions.Overflow e.Message)
-            | _ as e -> Error(Exceptions.Unknown e.Message)
+        try
+            Ok(Int32.Parse value)
+        with
+        | :? System.ArgumentNullException -> Error(ArgumentNullException())
+        | :? System.FormatException ->
+            Error(FormatException $"The input string '%s{value}' was not in a correct format.")
+        | :? System.OverflowException ->
+            Error(OverflowException "Value was either too large or too small for an Int32.")
+        | _ as e -> Error(ArgumentException e.Message)
